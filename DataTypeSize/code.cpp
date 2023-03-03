0a51ebbef5a570a9d8049180d80894165a5b9b71
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-03-02 20:28:06 -0400

    Create code.cpp

---
+++ DataTypeSize/code.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  
+{
+    cout<<"Size of Integer=4 bytes\nSize of Floating point=4 bytes\nSize of double=8 bytes\nSize of character=1 byte\nSize of Long integer=8 bytes\nSize of Short integer=2 bytes";
+}
