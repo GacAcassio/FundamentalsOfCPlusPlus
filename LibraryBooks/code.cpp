e89e18a3e76f133937c924c4e9659eeef3f60797
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-03-02 20:24:01 -0400

    Create code.cpp

---
+++ LibraryBooks/code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()   
+{
+    string st1, st2;
+    getline(cin, st1);
+    getline(cin,st2);
+    cout<<st1<<" book was written by "<<st2;
+}
